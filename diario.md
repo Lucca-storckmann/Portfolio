d00f168a0253900364a4d2005b4436b365838fac
Lucca Storckmann Medeiros <lucca.sto.medeiros@gmail.com>
2026-04-01 09:06:40 -0300

    att

---
+++ diario.md
@@ -0,0 +1,17 @@
+# Diário de Bordo de IA - [Lucca]
+
+## [01/04/2026]
+### Dificuldade encontrada
+O programa não estava reconhecendo a minha variável 
+
+### Prompt enviado para a IA
+Utilizei o proprio corretor do VSCode
+
+### Resposta da IA (Resumo)
+Adicionou o Int 
+
+### Minha análise e aprendizado
+Estava esquecendo de adicionar o comando Int que converte a String para um Num
+
+## [Insira outra data aqui]
+[...]
--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-comando_inicial = input("Digite algo aqui: ")
+while True:
+    comando_inicial = input("Digite algo aqui: ")
 
-if comando_inicial.upper() == "ABOUT":
-    print("Portfolio do Lucca")
-elif comando_inicial.upper() == "QUIT":
-    print("Saindo do programa")
+    if comando_inicial.upper() == "ABOUT":
+        print("Portfolio do Lucca")
+    elif comando_inicial.upper() == "ADD":
+        num_projetos = int(input("Quantos projeto deseja cadastrar: "))
+        if num_projetos <= 0:
+             print("Erro")
+        else:
+            for projeto in range(num_projetos):
+                nome_projeto = input("Qual o nome do projeto: ")
+                print("Projeto cadastrado.")
+    elif comando_inicial.upper() == "QUIT":
+        print("Saindo do programa")
+    break
 else:
     print("Erro na aplicação")
-print("Até logo")
+    print("Até logo")
